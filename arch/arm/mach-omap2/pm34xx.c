c3c993206f72dd9d848d02d8a34ba44c12c1959b
Kevin Hilman <khilman@deeprootsystems.com>
2009-11-23 11:05:02 -0800

    temp: OMAP3: PM: GPIO: disable PER UART after GPIO save
    FIXME: GPIO core needs to use clock API to prevent this.
    
    When PER UARTs are disabled in idle path and no GPIOs are in use,
    the PER block may go idle.  This causes the GPIO context save
    that happens right after UART disabling to fail with data aborts
    when accessing the GPIO regs in PER.

--- arch/arm/mach-omap2/pm34xx.c
+++ arch/arm/mach-omap2/pm34xx.c
@@ -393,7 +393,6 @@ void omap_sram_idle(void)
 
 	/* PER */
 	if (per_next_state < PWRDM_POWER_ON) {
-		omap_uart_prepare_idle(2);
 		omap2_gpio_prepare_for_idle(per_next_state);
 		if (per_next_state == PWRDM_POWER_OFF) {
 			if (core_next_state == PWRDM_POWER_ON) {
@@ -403,6 +402,7 @@ void omap_sram_idle(void)
 			} else
 				omap3_per_save_context();
 		}
+		omap_uart_prepare_idle(2);
 	}
 
 	if (pwrdm_read_pwrst(cam_pwrdm) == PWRDM_POWER_ON)
