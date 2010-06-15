135056ad9a85cf4c8dd26ca9266ae9e6243b6b25
Sriramakrishnan <srk@ti.com>
2010-06-15 18:24:52 +0530

    TI EMAC Driver : driver changes for supporting new IRQ ack logic
    The generic DaVinci EMAC driver has been modified for Interrupt
    ack logic to handle level interrupts.
    
    Signed-off-by: Sriramakrishnan <srk@ti.com>
    Signed-off-by: Sriramakrishnan A G <srk@ti.com>

--- drivers/net/davinci_emac.c
+++ drivers/net/davinci_emac.c
@@ -307,6 +307,9 @@ static const char emac_version_string[] = "TI DaVinci EMAC Linux v6.1";
 /* EMAC Stats Clear Mask */
 #define EMAC_STATS_CLR_MASK    (0xFFFFFFFF)
 
+/* EMAC MAX number of IRQ lines */
+#define MAX_MODULE_IRQS 4
+
 /* emac_priv: EMAC private data structure
  *
  * EMAC adapter private data structure
@@ -343,6 +346,9 @@ struct emac_priv {
 	/*platform specific members*/
 	void (*int_enable) (void);
 	void (*int_disable) (void);
+	/*snapshot of IRQs */
+	u32 irqs_table[MAX_MODULE_IRQS];
+	u32 num_irqs;
 };
 
 /* clock frequency for EMAC */
@@ -978,6 +984,7 @@ static irqreturn_t emac_irq(int irq, void *dev_id)
 {
 	struct net_device *ndev = (struct net_device *)dev_id;
 	struct emac_priv *priv = netdev_priv(ndev);
+	int i;
 
 	++priv->isr_count;
 	if (likely(netif_running(priv->ndev))) {
@@ -986,6 +993,10 @@ static irqreturn_t emac_irq(int irq, void *dev_id)
 	} else {
 		/* we are closing down, so dont process anything */
 	}
+
+	for (i = 0; i < priv->num_irqs; i++)
+		 disable_irq_nosync(priv->irqs_table[i]);
+
 	return IRQ_HANDLED;
 }
 
@@ -1410,8 +1421,12 @@ static int emac_poll(struct napi_struct *napi, int budget)
 					&emac_rxhost_errcodes[cause][0], ch);
 		}
 	} else if (num_rx_pkts < budget) {
+		int i;
+
 		napi_complete(napi);
 		emac_int_enable(priv);
+		for (i = 0; i < priv->num_irqs; i++)
+			enable_irq(priv->irqs_table[i]);
 	}
 
 	return num_rx_pkts;
@@ -1520,7 +1535,7 @@ static int emac_dev_open(struct net_device *ndev)
 	u32 cnt;
 	struct resource *res;
 	int q, m, ret;
-	int i = 0;
+	int i = 0, irq_num = 0;
 	int k = 0;
 	struct emac_priv *priv = netdev_priv(ndev);
 
@@ -1550,14 +1565,17 @@ static int emac_dev_open(struct net_device *ndev)
 
 	/* Request IRQ */
 
+	priv->num_irqs = 0;
 	while ((res = platform_get_resource(priv->pdev, IORESOURCE_IRQ, k))) {
 		for (i = res->start; i <= res->end; i++) {
 			if (request_irq(i, emac_irq, IRQF_DISABLED,
 					ndev->name, ndev))
 				goto rollback;
+			priv->irqs_table[irq_num++] = i;
 		}
 		k++;
 	}
+	priv->num_irqs = irq_num;
 
 	/* Start/Enable EMAC hardware */
 	emac_hw_enable(priv);
