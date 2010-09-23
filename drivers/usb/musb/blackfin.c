aa3300bb2d860610adce7a0bb43d82a639008640
Hema Kalliguddi <hemahk@ti.com>
2010-09-22 19:27:40 -0500

    usb: musb: remove board_data parameter from musb_platform_init()
    Removed the board_data parameter being
    passed to musb_platform_init function
    as board_data can be extracted from
    device structure which is already member
    of musb structure.
    
    Acked-by: Kevin Hilman <khilman@deeprootsystems.com>
    Cc: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Hema HK <hemahk@ti.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>
    Signed-off-by: Sriramakrishnan A G <srk@ti.com>

--- drivers/usb/musb/blackfin.c
+++ drivers/usb/musb/blackfin.c
@@ -362,7 +362,7 @@ static void musb_platform_reg_init(struct musb *musb)
 	SSYNC();
 }
 
-int __init musb_platform_init(struct musb *musb, void *board_data)
+int __init musb_platform_init(struct musb *musb)
 {
 
 	/*
