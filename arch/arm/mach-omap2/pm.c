acdb75dfcdc2fb60d31d03718e7bb1ad6ba29980
Thara Gopinath <thara@ti.com>
2010-09-25 18:21:26 +0530

    OMAP4: Add opp tables.
    This patch adds OPP tables for OMAP4. A new file
    opp44xx_data.c has been introduced to keep the OMAP4
    opp tables and the registeration of these tables with
    the generic opp framework.
    
    Signed-off-by: Thara Gopinath <thara@ti.com>

--- arch/arm/mach-omap2/pm.c
+++ arch/arm/mach-omap2/pm.c
@@ -209,6 +209,7 @@ static int __init omap2_common_pm_init(void)
 		omap2_set_init_voltage("mpu", "dpll1_ck", mpu_dev);
 		omap2_set_init_voltage("core", "l3_ick", l3_dev);
 	} else if (cpu_is_omap44xx()) {
+		omap4_pm_init_opp_table();
 		omap2_set_init_voltage("mpu", "dpll_mpu_ck", mpu_dev);
 		omap2_set_init_voltage("core", "l3_div_ck", l3_dev);
 		omap2_set_init_voltage("iva", "dpll_iva_m5x2_ck", iva_dev);
