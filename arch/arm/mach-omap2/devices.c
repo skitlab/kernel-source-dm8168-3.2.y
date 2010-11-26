a6ba2a847c3601e7741f84f274f4416a1119cffc
Hemant Pedanekar <hemantp@ti.com>
2010-11-26 17:03:40 +0530

    TI814X: devices: Skip initialization of SPI and MMC
    This patch updates device initialization to skip SPI and MMC inits to allow
    booting on TI814X. It actually updates cpu_is_ti816x checks with cpu_is_ti814x.
    
    In the absence of this patch, the kernel would crash at MMC init (and SPI init
    if MMC init is skipped) with "External precise abort" on respective module
    access.
    
    Signed-off-by: Hemant Pedanekar <hemantp@ti.com>
    Signed-off-by: Sriramakrishnan A G <srk@ti.com>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -425,7 +425,7 @@ static inline void omap2_mcspi4_init(void)
 
 static void omap_init_mcspi(void)
 {
-	if (cpu_is_ti816x())
+	if (cpu_is_ti81xx())
 		return;
 
 	if (cpu_is_omap44xx())
@@ -643,7 +643,7 @@ static void __init omap_hsmmc_reset(void)
 	u32 i, nr_controllers;
 	struct clk *iclk, *fclk;
 
-	if (cpu_is_omap242x() || cpu_is_ti816x())
+	if (cpu_is_omap242x() || cpu_is_ti81xx())
 		return;
 
 	nr_controllers = cpu_is_omap44xx() ? OMAP44XX_NR_MMC :
