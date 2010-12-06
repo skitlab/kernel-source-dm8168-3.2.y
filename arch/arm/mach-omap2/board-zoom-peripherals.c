fb1a2dfe3eb386c94f9040235da58203a80101c0
Ravi B <ravibabu@ti.com>
2010-12-06 18:33:10 +0530

    USB: gadget: g_ether: fix kernel panic in gether_setup
    Now tx queue of net_device is allocated in register_netdev,
    so we should touch it(netif_stop_queue) after register_netdev
    is OK.
    
    The patch fixes the kernel panic below:
    
    loading modules
    (usb_gadget_probe_driver+0xe8/0x2c0) [   30.177764] [<c031ad18>] \
    (usb_gadget_probe_driver+0x0/0x2c0) from [<bf00593c>] (usb_composite_probe+0x98/0x9c \
    [g_ether]) [   30.189239] [<bf0058a4>] (usb_composite_probe+0x0/0x9c [g_ether]) from \
    [<bf00b9d8>] (init+0x1c/0x24 [g_ether]) [   30.199737]  r5:000b9008 r4:000510e9
    [   30.203521] [<bf00b9bc>] (init+0x0/0x24 [g_ether]) from [<c00473dc>] \
    (do_one_initcall+0x3c/0x1c0) [   30.212890] [<c00473a0>] (do_one_initcall+0x0/0x1c0) \
    from [<c00bc9b8>] (sys_init_module+0xbc/0x1d0) [   30.222412] [<c00bc8fc>] \
    (sys_init_module+0x0/0x1d0) from [<c0053000>] (ret_fast_syscall+0x0/0x3c) [   \
    30.231811]  r7:00000080 r6:000510e9 r5:be991d14 r4:00097e8e [   30.237823] Code: \
    e58820d4 e59821c8 e10f1000 f10c0080 (e5923008) [   30.244354] ---[ end trace \
    e28ceaa3c9efe334 ]--- Segmentation fault
    
    Cc: David Brownell <dbrownell@users.sourceforge.net>
    Cc: Greg Kroah-Hartman <gregkh@suse.de>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Michal Nazarewicz <m.nazarewicz@samsung.com>
    Cc: Andy Shevchenko <ext-andriy.shevchenko@nokia.com>
    Cc: Marcel Holtmann <marcel@holtmann.org>
    Signed-off-by: Ming Lei <tom.leiming@gmail.com>
    Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>

--- arch/arm/mach-omap2/board-zoom-peripherals.c
+++ arch/arm/mach-omap2/board-zoom-peripherals.c
@@ -207,7 +207,7 @@ static struct omap2_hsmmc_info mmc[] = {
 	{
 		.name		= "internal",
 		.mmc		= 2,
-		.caps		= MMC_CAP_4_BIT_DATA | MMC_CAP_8_BIT_DATA,
+		/*.caps		= MMC_CAP_4_BIT_DATA | MMC_CAP_8_BIT_DATA,*/
 		.gpio_cd	= -EINVAL,
 		.gpio_wp	= -EINVAL,
 		.nonremovable	= true,
