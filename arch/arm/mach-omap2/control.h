8f099d6b4442da06a3011c39e7b2484a2c4f1a33
Paul Walmsley <paul@pwsan.com>
2010-12-14 12:48:32 -0700

    OMAP3: control/PRCM: add omap3_ctrl_write_boot_mode()
    Get rid of the open-coded scratchpad write in mach-omap2/prcm.c and
    replace it with an actual API, omap3_ctrl_write_boot_mode().  While
    there, get rid of the gratuitous omap_writel().
    
    There's not much documentation available for what should wind up in
    the scratchpad here, so more documentation would be appreciated.
    Also, at some point, we should formalize our treatment of the scratchpad;
    right now, accesses to the scratchpad are not well-documented.
    
    Signed-off-by: Paul Walmsley <paul@pwsan.com>
    Reviewed-by: Kevin Hilman <khilman@deeprootsystems.com>
    Tested-by: Kevin Hilman <khilman@deeprootsystems.com>

--- arch/arm/mach-omap2/control.h
+++ arch/arm/mach-omap2/control.h
@@ -350,6 +350,7 @@ extern u32 *get_es3_restore_pointer(void);
 extern u32 omap3_arm_context[128];
 extern void omap3_control_save_context(void);
 extern void omap3_control_restore_context(void);
+extern void omap3_ctrl_write_boot_mode(u8 bootmode);
 
 #else
 #define omap_ctrl_base_get()		0
