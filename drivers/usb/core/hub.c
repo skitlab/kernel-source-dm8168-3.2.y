77b15687278ce9780fbbc21062c6b5ad556a75cd
Ajay Kumar Gupta <ajay.gupta@ti.com>
2011-01-07 19:36:05 +0530

    usb: pm: disbale autosuspend in default mode
    Signed-off-by: Ajay Kumar Gupta <ajay.gupta@ti.com>
    Signed-off-by: Sriramakrishnan A G <srk@ti.com>

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -1234,7 +1234,7 @@ static int hub_probe(struct usb_interface *intf, const struct usb_device_id *id)
 	hdev = interface_to_usbdev(intf);
 
 	/* Hubs have proper suspend/resume support */
-	usb_enable_autosuspend(hdev);
+	usb_disable_autosuspend(hdev);
 
 	if (hdev->level == MAX_TOPO_LEVEL) {
 		dev_err(&intf->dev,
