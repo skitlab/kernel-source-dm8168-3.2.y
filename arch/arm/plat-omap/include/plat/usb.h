956c57c6cbb524fb88a999090fb817ec3bf36776
Vaibhav Hiremath <hvaibhav@ti.com>
2011-03-09 11:31:08 +0530

    usb: otg: Adding twl6030-usb transceiver driver for OMAP4430
    Adding the twl6030-usb transceiver support for OMAP4 musb driver.
    
    OMAP4 supports 2 types of transceiver interface.
    
    1. UTMI: The PHY is embedded within OMAP4. The transceiver functionality
    is split between the twl6030 PMIC chip and OMAP4430. The VBUS, ID pin
    sensing and OTG SRP generation part is integrated in TWL6030 and UTMI PHY
    functionality is embedded within the OMAP4430.
    
    There is no direct interactions between the MUSB controller and TWL6030
    chip to communicate the session-valid, session-end and ID-GND events.
    It has to be done through a software by setting/resetting bits in
    one of the control module register of OMAP4430 which in turn toggles
    the appropriate signals to MUSB controller.
    
    The internal transceiver has functional clocks and
    powerdown bits to powerdown the PHY for power saving.
    
    Since there is no option available for having 2 transceiver drivers
    for one USB controller, internal PHY specific APIs are passed through
    plaform_data function pointers to use in the twl6030-usb transceiver
    driver.
    
    2. ULPI interface is provided for off-chip transceivers.
    
    Signed-off-by: Hema HK <hemahk@ti.com>
    Cc: Tony Lindgren <tony@atomide.com>
    Cc: David Brownell <dbrownell@users.sourceforge.net>
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    Signed-off-by: Sriramakrishnan A G <srk@ti.com>

--- arch/arm/plat-omap/include/plat/usb.h
+++ arch/arm/plat-omap/include/plat/usb.h
@@ -86,6 +86,10 @@ extern void usb_ohci_init(const struct ohci_hcd_omap_platform_data *pdata);
 
 /* This is needed for OMAP3 errata 1.164: enabled autoidle can prevent sleep */
 extern void usb_musb_disable_autoidle(void);
+extern int omap4430_phy_power(struct device *dev, int ID, int on);
+extern int omap4430_phy_set_clk(struct device *dev, int on);
+extern int omap4430_phy_init(struct device *dev);
+extern int omap4430_phy_exit(struct device *dev);
 
 #endif
 
