6bf865f81fbd62d0089231c741f952c63907a7a1
Ravikumar Kattekola <a0131654@psplinux052.india.ti.com>
2011-06-30 16:21:42 +0530

    ti814x: clock: Added some more feilds to dpll_data structure
    This patch adds some more fields to dpll_data clock structure to support ADPLL
    configuration parameters.
    
    Signed-off-by: Ravikumar Kattekola <rk@ti.com>
    Signed-off-by: Ravikumar Kattekola <a0131654@psplinux052.india.ti.com>

--- arch/arm/plat-omap/include/plat/clock.h
+++ arch/arm/plat-omap/include/plat/clock.h
@@ -123,6 +123,21 @@ struct clksel {
  * @recal_en_bit: bitshift of the PRM_IRQENABLE_* bit for recalibration IRQs
  * @recal_st_bit: bitshift of the PRM_IRQSTATUS_* bit for recalibration IRQs
  * @flags: DPLL type/features (see below)
+ * @dpll_id: Integer value used for uniquely identifying an ADPLL
+ * @div_m2n_reg: register containing the DPLL M2 and N bitfields
+ * @div_m2_mask: mask of the DPLL M2 bitfield in @div_m2n_reg
+ * @div_n_mask: mask of the DPLL N bitfield in @div_m2n_reg
+ * @post_div_m2: fixed m2 value for this DPLL
+ * @frac_mult_reg: register containing the fractional part of multiplier(m)
+ * @frac_mult_mask: mask of the fractional part bitfield in @fract_mult_reg
+ * @last_rounded_frac_m: cache of the last Fract M result of _round_rate()
+ * @bypass_bit: bitshift for entering bypass mode in @control_reg
+ * @byp_clk_src_bit:bitshift for selecting source clk in bypass @control_reg
+ * @stby_ret_bit: bitshift for entering stand-by-ret mode in @control_reg
+ * @stop_mode_bit: bitshift for entering stop-mode in @control_reg
+ * @soft_reset_bit: bitshift for resetting the DPLL logic (TINITZ)@control_reg
+ * @load_mn_reg: register for loading M and N values (TENABLE)
+ * @load_m2n2_reg: register for loading M2 and N2 values (TENABLEDIV)
  *
  * Possible values for @flags:
  * DPLL_J_TYPE: "J-type DPLL" (only some 36xx, 4xxx DPLLs)
@@ -141,33 +156,52 @@ struct clksel {
  * can be placed into read-only space.
  */
 struct dpll_data {
-	void __iomem		*mult_div1_reg;
-	u32			mult_mask;
-	u32			div1_mask;
-	struct clk		*clk_bypass;
-	struct clk		*clk_ref;
-	void __iomem		*control_reg;
-	u32			enable_mask;
-	unsigned int		rate_tolerance;
-	unsigned long		last_rounded_rate;
-	u16			last_rounded_m;
-	u16			max_multiplier;
-	u8			last_rounded_n;
-	u8			min_divider;
-	u8			max_divider;
-	u8			modes;
-#if defined(CONFIG_ARCH_OMAP3) || defined(CONFIG_ARCH_OMAP4)
-	void __iomem		*autoidle_reg;
-	void __iomem		*idlest_reg;
-	u32			autoidle_mask;
-	u32			freqsel_mask;
-	u32			idlest_mask;
-	u32			dco_mask;
-	u32			sddiv_mask;
-	u8			auto_recal_bit;
-	u8			recal_en_bit;
-	u8			recal_st_bit;
-	u8			flags;
+	void __iomem	*mult_div1_reg;
+	u32		mult_mask;
+	u32		div1_mask;
+	struct clk	*clk_bypass;
+	struct clk	*clk_ref;
+	void __iomem	*control_reg;
+	u32		enable_mask;
+	unsigned int	rate_tolerance;
+	unsigned long	last_rounded_rate;
+	u16		last_rounded_m;
+	u16		max_multiplier;
+	u8		last_rounded_n;
+	u8		min_divider;
+	u8		max_divider;
+	u8		modes;
+#if (defined(CONFIG_ARCH_OMAP3) ||\
+	defined(CONFIG_ARCH_OMAP4) ||\
+	defined(CONFIG_ARCH_TI81XX))
+	void __iomem	*autoidle_reg;
+	void __iomem	*idlest_reg;
+	u32		autoidle_mask;
+	u32		freqsel_mask;
+	u32		idlest_mask;
+	u32		dco_mask;
+	u32		sddiv_mask;
+	u8		auto_recal_bit;
+	u8		recal_en_bit;
+	u8		recal_st_bit;
+	u8		flags;
+	/* data fields required by TI814X ADPLLs */
+	u8		dpll_id;
+	void __iomem	*div_m2n_reg;
+	u32		div_m2_mask;
+	u32		div_n_mask;
+	u8		pre_div_n;
+	u8		post_div_m2;
+	void __iomem	*frac_mult_reg;
+	u32		frac_mult_mask;
+	u32		last_rounded_frac_m;
+	u32		bypass_bit;
+	u8		byp_clk_src_bit;
+	u8		stby_ret_bit;
+	u8		stop_mode_bit;
+	u8		soft_reset_bit;
+	void __iomem	*load_mn_reg;
+	void __iomem	*load_m2n2_reg;
 #  endif
 };
 
