88cea0b61d03bc0ceddfbef843ab93ecaa69c3f8
Sriramakrishnan A G <srk@ti.com>
2011-07-01 16:06:23 +0530

    omap2 : i2c : Fix i2c NAK timeout issue
    Fix i2c NAK timeout issue.
    
    Signed-off-by: Sriramakrishnan A G <srk@ti.com>

--- drivers/i2c/busses/i2c-omap.c
+++ drivers/i2c/busses/i2c-omap.c
@@ -624,13 +624,6 @@ static int omap_i2c_xfer_msg(struct i2c_adapter *adap,
 	}
 
 	if (dev->cmd_err & OMAP_I2C_STAT_NACK) {
-		if (msg->flags & I2C_M_IGNORE_NAK)
-			return 0;
-		if (stop) {
-			w = omap_i2c_read_reg(dev, OMAP_I2C_CON_REG);
-			w |= OMAP_I2C_CON_STP;
-			omap_i2c_write_reg(dev, OMAP_I2C_CON_REG, w);
-		}
 		return -EREMOTEIO;
 	}
 	return -EIO;
@@ -839,7 +832,8 @@ complete:
 		if (stat & OMAP_I2C_STAT_NACK) {
 			err |= OMAP_I2C_STAT_NACK;
 			omap_i2c_write_reg(dev, OMAP_I2C_CON_REG,
-					   OMAP_I2C_CON_STP);
+				omap_i2c_read_reg(dev, OMAP_I2C_CON_REG) |
+					OMAP_I2C_CON_STP);
 		}
 		if (stat & OMAP_I2C_STAT_AL) {
 			dev_err(dev->dev, "Arbitration lost\n");
@@ -850,6 +844,10 @@ complete:
 			omap_i2c_ack_stat(dev, stat &
 				(OMAP_I2C_STAT_RRDY | OMAP_I2C_STAT_RDR |
 				OMAP_I2C_STAT_XRDY | OMAP_I2C_STAT_XDR));
+			omap_i2c_write_reg(dev, OMAP_I2C_BUF_REG,
+				omap_i2c_read_reg(dev, OMAP_I2C_BUF_REG) |
+				OMAP_I2C_BUF_RXFIF_CLR |
+				OMAP_I2C_BUF_TXFIF_CLR);
 			omap_i2c_complete_cmd(dev, err);
 			return IRQ_HANDLED;
 		}
