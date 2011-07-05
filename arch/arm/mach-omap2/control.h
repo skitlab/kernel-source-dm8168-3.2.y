audioscience/linux-omap3

TI816X: MAIN FAPLL addition to kernel clock tree
This patch adds the FAPLL structure which is required for FAPLL
configuration, also added some ti816x specific clock API's like
fapll_get_rate, fapll_enable, fapll_disable, etc and added some
extra members to clk struct which are needed for fapll configuration.

Also added MAIN fapll configuration to clock tree and coresponding
synthesizer clocks.

Signed-off-by: Anil Kumar Ch <anilkumar@ti.com>

arch/arm/mach-omap2/control.h
@@ -29,6 +29,8 @@
 		OMAP2_L4_IO_ADDRESS(OMAP243X_CTRL_BASE + (reg))
 #define OMAP343X_CTRL_REGADDR(reg)					\
 		OMAP2_L4_IO_ADDRESS(OMAP343X_CTRL_BASE + (reg))
+#define TI816X_CTRL_REGADDR(reg)					\
+		TI81XX_L4_SLOW_IO_ADDRESS(TI81XX_CTRL_BASE + (reg))
 #else
 #define OMAP242X_CTRL_REGADDR(reg)					\
 		OMAP2_L4_IO_ADDRESS(OMAP242X_CTRL_BASE + (reg))
@@ -264,6 +266,56 @@
 #define TI81XX_CONTROL_MAC_ID1_LO	(TI81XX_CONTROL_DEVCONF + 0x038)
 #define TI81XX_CONTROL_MAC_ID1_HI	(TI81XX_CONTROL_DEVCONF + 0x03c)
 #define TI816X_CONTROL_PCIE_CFG		(TI81XX_CONTROL_DEVCONF + 0x040)
+
+/* TI816X PLL Control register offsets */
+#define TI816X_MAINPLL_CTRL		TI816X_CTRL_REGADDR(0x0400)
+#define TI816X_MAINPLL_PWD		TI816X_CTRL_REGADDR(0x0404)
+#define TI816X_MAINPLL_FREQ1		TI816X_CTRL_REGADDR(0x0408)
+#define TI816X_MAINPLL_DIV1		TI816X_CTRL_REGADDR(0x040C)
+#define TI816X_MAINPLL_FREQ2		TI816X_CTRL_REGADDR(0x0410)
+#define TI816X_MAINPLL_DIV2		TI816X_CTRL_REGADDR(0x0414)
+#define TI816X_MAINPLL_FREQ3		TI816X_CTRL_REGADDR(0x0418)
+#define TI816X_MAINPLL_DIV3		TI816X_CTRL_REGADDR(0x041C)
+#define TI816X_MAINPLL_FREQ4		TI816X_CTRL_REGADDR(0x0420)
+#define TI816X_MAINPLL_DIV4		TI816X_CTRL_REGADDR(0x0424)
+#define TI816X_MAINPLL_FREQ5		TI816X_CTRL_REGADDR(0x0428)
+#define TI816X_MAINPLL_DIV5		TI816X_CTRL_REGADDR(0x042C)
+#define TI816X_MAINPLL_DIV6		TI816X_CTRL_REGADDR(0x0434)
+#define TI816X_MAINPLL_DIV7		TI816X_CTRL_REGADDR(0x043C)
+
+#define TI816X_DDRPLL_CTRL		TI816X_CTRL_REGADDR(0x0440)
+#define TI816X_DDRPLL_PWD		TI816X_CTRL_REGADDR(0x0444)
+#define TI816X_DDRPLL_DIV1		TI816X_CTRL_REGADDR(0x044C)
+#define TI816X_DDRPLL_FREQ2		TI816X_CTRL_REGADDR(0x0450)
+#define TI816X_DDRPLL_DIV2		TI816X_CTRL_REGADDR(0x0454)
+#define TI816X_DDRPLL_FREQ3		TI816X_CTRL_REGADDR(0x0458)
+#define TI816X_DDRPLL_DIV3		TI816X_CTRL_REGADDR(0x045C)
+#define TI816X_DDRPLL_FREQ4		TI816X_CTRL_REGADDR(0x0460)
+#define TI816X_DDRPLL_DIV4		TI816X_CTRL_REGADDR(0x0464)
+#define TI816X_DDRPLL_FREQ5		TI816X_CTRL_REGADDR(0x0468)
+#define TI816X_DDRPLL_DIV5		TI816X_CTRL_REGADDR(0x046C)
+
+#define TI816X_DDR_RCD			TI816X_CTRL_REGADDR(0x070C)
+
+#define TI816X_VIDEOPLL_CTRL		TI816X_CTRL_REGADDR(0x0470)
+#define TI816X_VIDEOPLL_PWD		TI816X_CTRL_REGADDR(0x0474)
+#define TI816X_VIDEOPLL_FREQ1		TI816X_CTRL_REGADDR(0x0478)
+#define TI816X_VIDEOPLL_DIV1		TI816X_CTRL_REGADDR(0x047C)
+#define TI816X_VIDEOPLL_FREQ2		TI816X_CTRL_REGADDR(0x0480)
+#define TI816X_VIDEOPLL_DIV2		TI816X_CTRL_REGADDR(0x0484)
+#define TI816X_VIDEOPLL_FREQ3		TI816X_CTRL_REGADDR(0x0488)
+#define TI816X_VIDEOPLL_DIV3		TI816X_CTRL_REGADDR(0x048C)
+
+#define TI816X_AUDIOPLL_CTRL		TI816X_CTRL_REGADDR(0x04A0)
+#define TI816X_AUDIOPLL_PWD		TI816X_CTRL_REGADDR(0x04A4)
+#define TI816X_AUDIOPLL_FREQ2		TI816X_CTRL_REGADDR(0x04B0)
+#define TI816X_AUDIOPLL_DIV2		TI816X_CTRL_REGADDR(0x04B4)
+#define TI816X_AUDIOPLL_FREQ3		TI816X_CTRL_REGADDR(0x04B8)
+#define TI816X_AUDIOPLL_DIV3		TI816X_CTRL_REGADDR(0x04BC)
+#define TI816X_AUDIOPLL_FREQ4		TI816X_CTRL_REGADDR(0x04C0)
+#define TI816X_AUDIOPLL_DIV4		TI816X_CTRL_REGADDR(0x04C4)
+#define TI816X_AUDIOPLL_FREQ5		TI816X_CTRL_REGADDR(0x04C8)
+#define TI816X_AUDIOPLL_DIV5		TI816X_CTRL_REGADDR(0x04CC)
 /* TODO: Add other ti81xx registers... */
 
 /*

skitlab/kernel-source-dm8168-3.2.y

Signed-off-by: Sriramakrishnan A G <srk@ti.com>

arch/arm/mach-omap2/control.h
@@ -266,6 +266,56 @@
 #define TI81XX_CONTROL_MAC_ID1_LO	(TI81XX_CONTROL_DEVCONF + 0x038)
 #define TI81XX_CONTROL_MAC_ID1_HI	(TI81XX_CONTROL_DEVCONF + 0x03c)
 #define TI816X_CONTROL_PCIE_CFG		(TI81XX_CONTROL_DEVCONF + 0x040)
+
+/* TI816X PLL Control register offsets */
+#define TI816X_MAINPLL_CTRL		TI81XX_CTRL_REGADDR(0x0400)
+#define TI816X_MAINPLL_PWD		TI81XX_CTRL_REGADDR(0x0404)
+#define TI816X_MAINPLL_FREQ1		TI81XX_CTRL_REGADDR(0x0408)
+#define TI816X_MAINPLL_DIV1		TI81XX_CTRL_REGADDR(0x040C)
+#define TI816X_MAINPLL_FREQ2		TI81XX_CTRL_REGADDR(0x0410)
+#define TI816X_MAINPLL_DIV2		TI81XX_CTRL_REGADDR(0x0414)
+#define TI816X_MAINPLL_FREQ3		TI81XX_CTRL_REGADDR(0x0418)
+#define TI816X_MAINPLL_DIV3		TI81XX_CTRL_REGADDR(0x041C)
+#define TI816X_MAINPLL_FREQ4		TI81XX_CTRL_REGADDR(0x0420)
+#define TI816X_MAINPLL_DIV4		TI81XX_CTRL_REGADDR(0x0424)
+#define TI816X_MAINPLL_FREQ5		TI81XX_CTRL_REGADDR(0x0428)
+#define TI816X_MAINPLL_DIV5		TI81XX_CTRL_REGADDR(0x042C)
+#define TI816X_MAINPLL_DIV6		TI81XX_CTRL_REGADDR(0x0434)
+#define TI816X_MAINPLL_DIV7		TI81XX_CTRL_REGADDR(0x043C)
+
+#define TI816X_DDRPLL_CTRL		TI81XX_CTRL_REGADDR(0x0440)
+#define TI816X_DDRPLL_PWD		TI81XX_CTRL_REGADDR(0x0444)
+#define TI816X_DDRPLL_DIV1		TI81XX_CTRL_REGADDR(0x044C)
+#define TI816X_DDRPLL_FREQ2		TI81XX_CTRL_REGADDR(0x0450)
+#define TI816X_DDRPLL_DIV2		TI81XX_CTRL_REGADDR(0x0454)
+#define TI816X_DDRPLL_FREQ3		TI81XX_CTRL_REGADDR(0x0458)
+#define TI816X_DDRPLL_DIV3		TI81XX_CTRL_REGADDR(0x045C)
+#define TI816X_DDRPLL_FREQ4		TI81XX_CTRL_REGADDR(0x0460)
+#define TI816X_DDRPLL_DIV4		TI81XX_CTRL_REGADDR(0x0464)
+#define TI816X_DDRPLL_FREQ5		TI81XX_CTRL_REGADDR(0x0468)
+#define TI816X_DDRPLL_DIV5		TI81XX_CTRL_REGADDR(0x046C)
+
+#define TI816X_DDR_RCD			TI81XX_CTRL_REGADDR(0x070C)
+
+#define TI816X_VIDEOPLL_CTRL		TI81XX_CTRL_REGADDR(0x0470)
+#define TI816X_VIDEOPLL_PWD		TI81XX_CTRL_REGADDR(0x0474)
+#define TI816X_VIDEOPLL_FREQ1		TI81XX_CTRL_REGADDR(0x0478)
+#define TI816X_VIDEOPLL_DIV1		TI81XX_CTRL_REGADDR(0x047C)
+#define TI816X_VIDEOPLL_FREQ2		TI81XX_CTRL_REGADDR(0x0480)
+#define TI816X_VIDEOPLL_DIV2		TI81XX_CTRL_REGADDR(0x0484)
+#define TI816X_VIDEOPLL_FREQ3		TI81XX_CTRL_REGADDR(0x0488)
+#define TI816X_VIDEOPLL_DIV3		TI81XX_CTRL_REGADDR(0x048C)
+
+#define TI816X_AUDIOPLL_CTRL		TI81XX_CTRL_REGADDR(0x04A0)
+#define TI816X_AUDIOPLL_PWD		TI81XX_CTRL_REGADDR(0x04A4)
+#define TI816X_AUDIOPLL_FREQ2		TI81XX_CTRL_REGADDR(0x04B0)
+#define TI816X_AUDIOPLL_DIV2		TI81XX_CTRL_REGADDR(0x04B4)
+#define TI816X_AUDIOPLL_FREQ3		TI81XX_CTRL_REGADDR(0x04B8)
+#define TI816X_AUDIOPLL_DIV3		TI81XX_CTRL_REGADDR(0x04BC)
+#define TI816X_AUDIOPLL_FREQ4		TI81XX_CTRL_REGADDR(0x04C0)
+#define TI816X_AUDIOPLL_DIV4		TI81XX_CTRL_REGADDR(0x04C4)
+#define TI816X_AUDIOPLL_FREQ5		TI81XX_CTRL_REGADDR(0x04C8)
+#define TI816X_AUDIOPLL_DIV5		TI81XX_CTRL_REGADDR(0x04CC)
 /* TODO: Add other ti81xx registers... */
 
 /*