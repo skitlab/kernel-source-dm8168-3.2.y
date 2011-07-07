9f8d7b2a15597d28ec68cd5620ef7ad4fd077ab9
Sriramakrishnan A G <srk@ti.com>
2011-07-07 20:18:46 +0530

    ti814x : cpsw : set phy address based on board type
    The PHY address for cpsw ports are dependent on the board version
    Initialize the PHY address based on Si version detected at runtime
    
    Signed-off-by: Sriramakrishnan A G <srk@ti.com>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -1859,6 +1859,13 @@ void ti814x_cpsw_init(void)
 #if 0
 	ti814x_cpsw_mux();
 #endif
+	if (omap_rev() == TI8148_REV_ES1_0)
+		cpsw_slaves[0].phy_id = "0:01";
+	else {
+		cpsw_slaves[0].phy_id = "0:00";
+		cpsw_slaves[1].phy_id = "0:01";
+	}
+
 	platform_device_register(&cpsw_mdio_device);
 	platform_device_register(&ti814x_cpsw_device);
 	clk_add_alias(NULL, dev_name(&cpsw_mdio_device.dev),
