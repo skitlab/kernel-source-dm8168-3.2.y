dceca30b7cbf3d7ab0d626233432bfbe24b72ec7
Mugunthan V N <mugunthanvnm@ti.com>
2011-09-22 19:13:15 +0530

    ti814x: cpsw: fix for proper mac id used from e-fuse
    * Fix for proper mac id use in cpsw ethernet interface from e-fuse
    
    Signed-off-by: Mugunthan V N <mugunthanvnm@ti.com>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -2188,6 +2188,7 @@ void ti814x_cpsw_init(void)
 	cpsw_slaves[1].mac_addr[3] = (mac_hi & 0xFF000000) >> 24;
 	cpsw_slaves[1].mac_addr[4] = mac_lo & 0xFF;
 	cpsw_slaves[1].mac_addr[5] = (mac_lo & 0xFF00) >> 8;
+	memcpy(ti814x_cpsw_pdata.mac_addr, cpsw_slaves[0].mac_addr, ETH_ALEN);
 #if 0
 	ti814x_cpsw_mux();
 #endif
