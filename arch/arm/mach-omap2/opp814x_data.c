3b269ace5ad803a4a69efa2245d3e1a7447b077e
Ravikumar Kattekola <rk@ti.com>
2011-11-15 14:56:38 +0530

    ti814x: PM: cpufreq: Add OPP data
    This patch adds OPP data for arm(mpu),l3_slow and l4_slow(core) of ti814x.
    This patch enables Kconfig option to use OPP layer library.
    We need CONFIG_PM_OPP flag to be enabled to use OPP layer library
    which depends on CONFIG_ARCH_HAS_OPP.There is an existing menu config
    item under"Power management options" for this flag which has no prompt value.
    Following prompt value has been added :
    	Power management options --->
    	 [] Architecure Supports multiple OPPs
    this activates following item:
             [] Operating Performance Point (OPP) Layer library
    to use OPP layer library both the above menu items must be enabled.
    
    Signed-off-by: Ravikumar Kattekola <rk@ti.com>

---
+++ arch/arm/mach-omap2/opp814x_data.c
@@ -0,0 +1,64 @@
+/*
+ * TI81XX Power Management
+ *
+ * This module implements TI81XX specific Power management Routines
+ *
+ * Copyright (C) {2011} Texas Instruments Incorporated - http://www.ti.com/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation version 2.
+ *
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/module.h>
+#include <linux/opp.h>
+
+#include <plat/cpu.h>
+#include <plat/omap_device.h>
+
+#include "omap_opp_data.h"
+
+static struct omap_opp_def __initdata ti814x_opp_def_list[] = {
+	/* MPU OPP1 - OPP 50 */
+	OPP_INITIALIZER("mpu", true, 300000000, 950000),
+	/* MPU OPP2 - OPP 100 */
+	OPP_INITIALIZER("mpu", true, 600000000, 1100000),
+	/* MPU OPP3 - OPP 120 */
+	OPP_INITIALIZER("mpu", true, 720000000, 1200000),
+
+	/* L3 OPP1 - OPP 50 */
+	OPP_INITIALIZER("l3_slow", true, 100000000, 950000),
+	/* L3 OPP2 - OPP 100 */
+	OPP_INITIALIZER("l3_slow", true, 200000000, 1100000),
+	/* L3 OPP3 - OPP 120 */
+	OPP_INITIALIZER("l3_slow", true, 240000000, 1200000),
+
+	/* L4 OPP1 - OPP 50 */
+	OPP_INITIALIZER("l4_slow", true, 100000000, 950000),
+	/* L4 OPP2 - OPP 100 */
+	OPP_INITIALIZER("l4_slow", true, 200000000, 1100000),
+	/* L4 OPP3 - OPP 120 */
+	OPP_INITIALIZER("l4_slow", true, 240000000, 1200000),
+};
+
+/**
+ * ti814x_opp_init() - initialize ti814x opp table
+ */
+static int __init ti814x_opp_init(void)
+{
+	int r = -ENODEV;
+
+	if (!cpu_is_ti814x())
+		return r;
+
+	r = omap_init_opp_table(ti814x_opp_def_list,
+			ARRAY_SIZE(ti814x_opp_def_list));
+
+	return r;
+}
+device_initcall(ti814x_opp_init);
