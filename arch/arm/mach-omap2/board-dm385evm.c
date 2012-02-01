3064bce8324f67a6ac64b97ddf89e92b8aaf22b3
Saxena, Parth <parth.saxena@ti.com>
2012-02-01 16:34:36 +0530

    ti81xx: nand: set buswidth correctly for 8/16 bit nands
    This patch reverses the buswidth detection switch setting
    for detecting 8/16 bit nands, so as to make the switch
    setting compatible with the switch documentation.
    
    Signed-off-by: Saxena, Parth <parth.saxena@ti.com>

--- arch/arm/mach-omap2/board-dm385evm.c
+++ arch/arm/mach-omap2/board-dm385evm.c
@@ -528,9 +528,9 @@ static void __init dm385_evm_init(void)
 	if (cpu_is_ti814x()) {
 		u32 *control_status = TI81XX_CTRL_REGADDR(0x40);
 		if (*control_status & (1<<16))
-			bw = 0; /*8-bit nand if BTMODE BW pin on board is ON*/
+			bw = 2; /*16-bit nand if BTMODE BW pin on board is ON*/
 		else
-			bw = 2; /*16-bit nand if BTMODE BW pin on board is OFF*/
+			bw = 0; /*8-bit nand if BTMODE BW pin on board is OFF*/
 
 		board_nand_init(ti814x_nand_partitions,
 			ARRAY_SIZE(ti814x_nand_partitions), 0, bw);
