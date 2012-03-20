dd7bc15e62e1cd14ba9320f5367287ac31296db3
Mugunthan V N <mugunthanvnm@ti.com>
2012-03-20 16:53:25 +0530

    ti81xx: cpsw: Enable ifctl_a of Mac Control reg for RMII/RGMII Gasket Control
    Enable ifctl_a of Mac Control register for 100Mbps to enable RMII/RGMII
    Gasket control
    
    Signed-off-by: Mugunthan V N <mugunthanvnm@ti.com>

--- drivers/net/cpsw.c
+++ drivers/net/cpsw.c
@@ -769,6 +769,8 @@ static void _cpsw_adjust_link(struct cpsw_slave *slave,
 
 		if (phy->speed == 10)
 			mac_control |= BIT(18);
+		else if (phy->speed == 100)
+			mac_control |= BIT(15);
 		else if (phy->speed == 1000)
 			mac_control |= BIT(7);	/* GIGABITEN	*/
 		if (phy->duplex)
