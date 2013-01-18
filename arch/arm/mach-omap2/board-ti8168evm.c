ff6a9d36ecfb4f234be75e2cdbc64083cad13511
Pavel Nakonechny <pavel.nakonechny@skitlab.ru>
2013-01-18 16:05:23 +0400

    ti8168evm: add GPIO voltage regulator

--- arch/arm/mach-omap2/board-ti8168evm.c
+++ arch/arm/mach-omap2/board-ti8168evm.c
@@ -15,6 +15,13 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+#ifdef CONFIG_REGULATOR_GPIO
+#include <linux/gpio.h>
+#include <linux/regulator/driver.h>
+#include <linux/regulator/machine.h>
+#include <linux/regulator/gpio-regulator.h>
+#endif
+
 #include <mach/hardware.h>
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
@@ -69,6 +76,89 @@ static struct omap2_hsmmc_info mmc[] = {
 	{}	/* Terminator */
 };
 
+#ifdef CONFIG_REGULATOR_GPIO
+static struct regulator_consumer_supply ti816x_gpio_dcdc_supply[] = {
+	{
+		.supply = "vdd_avs",
+	},
+};
+
+static struct regulator_init_data ti816x_gpio_vr_init_data = {
+	.constraints = {
+		.min_uV		= 800000,
+		.max_uV		= 1025000,
+		.valid_ops_mask	= (REGULATOR_CHANGE_VOLTAGE |
+						   REGULATOR_CHANGE_STATUS),
+	},
+	.consumer_supplies	= ti816x_gpio_dcdc_supply,
+	.num_consumer_supplies	= ARRAY_SIZE(ti816x_gpio_dcdc_supply),
+};
+
+/* Supported voltage values for regulators */
+static struct gpio_regulator_state ti816x_states_table[] = {
+	{ .value = 800000, .gpios = 0x0 },
+	{ .value = 815000, .gpios = 0x8 },
+	{ .value = 830000, .gpios = 0x4 },
+	{ .value = 845000, .gpios = 0xC },
+	{ .value = 860000, .gpios = 0x2 },
+	{ .value = 875000, .gpios = 0xA },
+	{ .value = 890000, .gpios = 0x6 },
+	{ .value = 905000, .gpios = 0xE },
+	{ .value = 920000, .gpios = 0x1 },
+	{ .value = 935000, .gpios = 0x9 },
+	{ .value = 950000, .gpios = 0x5 },
+	{ .value = 965000, .gpios = 0xD },
+	{ .value = 980000, .gpios = 0x3 },
+	{ .value = 995000, .gpios = 0xB },
+	{ .value = 1010000, .gpios = 0x7},
+	{ .value = 1025000, .gpios = 0xF},
+};
+
+/* Macro for GPIO voltage regulator */
+#define VR_GPIO_INSTANCE	0
+
+static struct gpio ti816x_vcore_gpios[] = {
+	{ (VR_GPIO_INSTANCE * 32) + 0, GPIOF_OUT_INIT_HIGH /* GPIOF_IN */, "vgpio 0"},
+	{ (VR_GPIO_INSTANCE * 32) + 1, GPIOF_OUT_INIT_HIGH /* GPIOF_IN */, "vgpio 1"},
+	{ (VR_GPIO_INSTANCE * 32) + 2, GPIOF_OUT_INIT_HIGH /* GPIOF_IN */, "vgpio 2"},
+	{ (VR_GPIO_INSTANCE * 32) + 3, GPIOF_OUT_INIT_HIGH /* GPIOF_IN */, "vgpio 3"},
+};
+
+/* GPIO regulator platform data */
+static struct gpio_regulator_config ti816x_gpio_vr_config = {
+	.supply_name	= "VFB",
+	.enable_gpio	= -EINVAL,
+	.enable_high	= 1,
+	.enabled_at_boot = 1,
+	.startup_delay	= 0,
+	.gpios			= ti816x_vcore_gpios,
+	.nr_gpios		= ARRAY_SIZE(ti816x_vcore_gpios),
+	.states			= ti816x_states_table,
+	.nr_states		= ARRAY_SIZE(ti816x_states_table),
+	.type			= REGULATOR_VOLTAGE,
+	.init_data		= &ti816x_gpio_vr_init_data,
+};
+
+/* VCORE for SR regulator init */
+static struct platform_device ti816x_gpio_vr_device = {
+	.name	= "gpio-regulator",
+	.id		= -1,
+	.dev = {
+		.platform_data = &ti816x_gpio_vr_config,
+	},
+};
+
+static void __init ti816x_gpio_vr_init(void)
+{
+	if (platform_device_register(&ti816x_gpio_vr_device))
+		pr_err("failed to register ti816x_gpio_vr device\n");
+	else
+		pr_info("registered ti816x_gpio_vr device\n");
+}
+#else
+static inline void ti816x_gpio_vr_init(void) {}
+#endif
+
 static void __init ti81xx_evm_init(void)
 {
 	ti81xx_mux_init(board_mux);
@@ -77,6 +167,13 @@ static void __init ti81xx_evm_init(void)
 	omap_board_config = ti81xx_evm_config;
 	omap_board_config_size = ARRAY_SIZE(ti81xx_evm_config);
 	omap2_hsmmc_init(mmc);
+#ifdef CONFIG_REGULATOR_GPIO
+	if (cpu_is_ti816x()) {
+		ti816x_gpio_vr_init();
+		regulator_has_full_constraints();
+		regulator_use_dummy_regulator();
+	}
+#endif
 }
 
 MACHINE_START(TI8168EVM, "ti8168evm")
