925acc45d91f2c5807427c4d642c0520128e5fcc
Aida Mynzhasova <aida.mynzhasova@skitlab.ru>
2013-03-29 11:56:27 +0400

    media: ti816x: V4L2 capture and display drivers were added for DM816x SoC

--- arch/arm/mach-omap2/board-ti8168evm.c
+++ arch/arm/mach-omap2/board-ti8168evm.c
@@ -29,6 +29,8 @@
 #include <linux/clk.h>
 #include <linux/err.h>
 #include <linux/ti81xxfb.h>
+#include <linux/ti81xx.h>
+#include <linux/vps_capture.h>
 #include <sound/tlv320aic3x.h>
 
 #include <mach/hardware.h>
@@ -503,6 +505,234 @@ void ti81xxfb_set_platform_data(struct ti81xxfb_platform_data *data)
 }
 #endif
 
+#if defined(CONFIG_VIDEO_TI81XX_VIDOUT_MODULE) || defined(CONFIG_VIDEO_TI81XX_VIDOUT)
+static struct resource ti81xx_vidout_resource[VPS_DISPLAY_INST_MAX] = {
+};
+
+static struct platform_device ti81xx_vidout_device = {
+	.name		= "ti81xx_vidout",
+	.num_resources  = ARRAY_SIZE(ti81xx_vidout_resource),
+	.resource       = &ti81xx_vidout_resource[0],
+	.id             = -1,
+};
+
+static int __init ti81xx_vout_init(void)
+{
+	int r;
+
+	r = platform_device_register(&ti81xx_vidout_device);
+	if (r)
+		printk(KERN_ERR "Unable to register ti81xx_vidout device\n");
+	else
+		printk(KERN_INFO "registered ti81xx_vidout device\n");
+	return r;
+}
+#else
+static int __init ti81xx_vout_init(void)
+{
+	return 0;
+}
+#endif
+
+#if defined(CONFIG_VIDEO_TI81XX_VIDIN_MODULE) || defined(CONFIG_VIDEO_TI81XX_VIDIN)
+
+#define HDVPSS_CAPTURE_INST0_BASE	0x48105500
+#define HDVPSS_CAPTURE_INST0_SIZE	1024u
+
+#define HDVPSS_CAPTURE_INST2_BASE	0x48105A00
+#define HDVPSS_CAPTURE_INST2_SIZE	1024u
+
+#if defined (CONFIG_TI816X_THS7368)
+int pcf8575_ths7368_select_video_decoder(int vid_decoder_id);
+int pcf8575_ths7368_set_tvp7002_filter(enum fvid2_standard standard);
+#else
+int pcf8575_ths7368_select_video_decoder(int vid_decoder_id)
+{
+	return 0;
+}
+int pcf8575_ths7368_set_tvp7002_filter(enum fvid2_standard standard)
+{
+	return 0;
+}
+#endif
+
+u8 ti81xx_card_name[] = "TI81xx_catalogue";
+struct ti81xxvin_interface tvp7002_pdata = {
+	.clk_polarity = 0,
+	.hs_polarity = 0,
+	.vs_polarity = 1,
+	.fid_polarity = 0,
+	.sog_polarity = 0,
+
+};
+static struct ti81xxvin_subdev_info hdvpss_capture_sdev_info[] = {
+	{
+		.name	= TVP7002_INST0,
+		.board_info = {
+			/* TODO Find the correct address
+				of the TVP7002 connected */
+			I2C_BOARD_INFO("tvp7002", 0x5d),
+			.platform_data = &tvp7002_pdata,
+		},
+		.vip_port_cfg = {
+			.ctrlChanSel = VPS_VIP_CTRL_CHAN_SEL_15_8,
+			.ancChSel8b = VPS_VIP_ANC_CH_SEL_DONT_CARE,
+			.pixClkEdgePol = VPS_VIP_PIX_CLK_EDGE_POL_RISING,
+			.invertFidPol = 0,
+			.embConfig = {
+				.errCorrEnable = 1,
+				.srcNumPos = VPS_VIP_SRC_NUM_POS_DONT_CARE,
+				.isMaxChan3Bits = 0,
+			},
+			.disConfig = {
+				.fidSkewPostCnt = 0,
+				.fidSkewPreCnt = 0,
+				.lineCaptureStyle =
+					VPS_VIP_LINE_CAPTURE_STYLE_DONT_CARE,
+				.fidDetectMode =
+					VPS_VIP_FID_DETECT_MODE_DONT_CARE,
+				.actvidPol = VPS_VIP_POLARITY_DONT_CARE,
+				.vsyncPol =  VPS_VIP_POLARITY_DONT_CARE,
+				.hsyncPol = VPS_VIP_POLARITY_DONT_CARE,
+			}
+		},
+		.video_capture_mode =
+		   VPS_CAPT_VIDEO_CAPTURE_MODE_SINGLE_CH_NON_MUX_EMBEDDED_SYNC,
+		.video_if_mode = VPS_CAPT_VIDEO_IF_MODE_16BIT,
+		.input_data_format = FVID2_DF_YUV422P,
+		.ti81xxvin_select_decoder =	pcf8575_ths7368_select_video_decoder,
+		.ti81xxvin_set_mode = pcf8575_ths7368_set_tvp7002_filter,
+		.decoder_id = 0,
+	},
+};
+
+static const struct v4l2_dv_preset hdvpss_inst0_inp0_presets[] = {
+	{
+		.preset = V4L2_DV_720P60,
+	},
+	{
+		.preset = V4L2_DV_1080I60,
+	},
+	{
+		.preset = V4L2_DV_1080P60,
+	},
+	{
+		.preset = V4L2_DV_1080P30,
+	},
+};
+
+static const struct v4l2_dv_preset hdvpss_inst2_inp0_presets[] = {
+	{
+		.preset = V4L2_DV_720P60,
+	},
+	{
+		.preset = V4L2_DV_1080I60,
+	},
+	{
+		.preset = V4L2_DV_1080P60,
+	},
+	{
+		.preset = V4L2_DV_1080P30,
+	},
+};
+
+static const struct ti81xxvin_input hdvpss_inst0_inputs[] = {
+	{
+		.input = {
+			.index		= 0,
+			.name		= "Component",
+			.type		= V4L2_INPUT_TYPE_CAMERA,
+			.std		= V4L2_STD_UNKNOWN,
+			.capabilities	= V4L2_OUT_CAP_PRESETS,
+		},
+		.subdev_name	= TVP7002_INST0,
+		.dv_presets	= hdvpss_inst0_inp0_presets,
+		.num_dv_presets	= ARRAY_SIZE(hdvpss_inst0_inp0_presets),
+	},
+};
+
+static const struct ti81xxvin_input hdvpss_inst1_inputs[] = {
+	{
+		.input = {
+			.index		= 0,
+			.name		= "Component",
+			.type		= V4L2_INPUT_TYPE_CAMERA,
+			.std		= V4L2_STD_UNKNOWN,
+			.capabilities	= V4L2_OUT_CAP_PRESETS,
+		},
+		.subdev_name	= TVP7002_INST1,
+		.dv_presets	= hdvpss_inst2_inp0_presets,
+		.num_dv_presets	= ARRAY_SIZE(hdvpss_inst2_inp0_presets),
+	},
+};
+
+static struct ti81xxvin_config ti81xx_hsvpss_capture_cfg = {
+	.subdev_info = hdvpss_capture_sdev_info,
+	.subdev_count = ARRAY_SIZE(hdvpss_capture_sdev_info),
+	.card_name = ti81xx_card_name,
+	.inst_config[0] = {
+		.inputs = hdvpss_inst0_inputs,
+		.input_count = ARRAY_SIZE(hdvpss_inst0_inputs),
+	},
+	.inst_config[1] = {
+		.inputs = hdvpss_inst0_inputs,
+		.input_count = 0,
+	},
+	.inst_config[2] = {
+		.inputs = hdvpss_inst1_inputs,
+		.input_count = ARRAY_SIZE(hdvpss_inst1_inputs),
+	},
+	.inst_config[3] = {
+		.inputs = hdvpss_inst1_inputs,
+		.input_count = 0,
+	},
+};
+
+static struct resource ti81xx_hdvpss_capture_resource[] = {
+	[0] = {
+		.start = HDVPSS_CAPTURE_INST0_BASE,
+		.end   = (HDVPSS_CAPTURE_INST0_BASE +
+				HDVPSS_CAPTURE_INST0_SIZE - 1),
+		.flags = IORESOURCE_MEM,
+	},
+	[1] = {
+		.start = HDVPSS_CAPTURE_INST2_BASE,
+		.end   = (HDVPSS_CAPTURE_INST2_BASE +
+				HDVPSS_CAPTURE_INST2_SIZE - 1),
+		.flags = IORESOURCE_MEM,
+	},
+};
+
+static struct platform_device hdvpss_capture_dev = {
+	.name		= "ti81xxvin",
+	.id		= -1,
+	.dev		= {
+			.dma_mask		= &ti81xx_dma_mask,
+			.coherent_dma_mask	= ~(u32)0,
+	},
+	.num_resources = 2,
+	.resource = ti81xx_hdvpss_capture_resource,
+};
+
+static int __init ti81xx_vin_init(void)
+{
+	int r;
+	hdvpss_capture_dev.dev.platform_data = &ti81xx_hsvpss_capture_cfg;
+	r = platform_device_register(&hdvpss_capture_dev);
+	if (r)
+		printk(KERN_ERR "unable to register ti81xx_vin device\n");
+	else
+		printk(KERN_INFO "registered ti81xx_vin device\n");
+	return r;
+
+}
+#else
+static int __init ti81xx_vin_init(void)
+{
+	return 0;
+}
+#endif
+
 static void __init ti81xx_evm_init(void)
 {
 	ti81xx_mux_init(board_mux);
@@ -529,6 +759,8 @@ static void __init ti81xx_evm_init(void)
 	ti81xx_vpss_init();
 	ti81xx_hdmi_init();
 	ti81xx_fb_init();
+	ti81xx_vout_init();
+	ti81xx_vin_init();
 }
 
 MACHINE_START(TI8168EVM, "ti8168evm")
